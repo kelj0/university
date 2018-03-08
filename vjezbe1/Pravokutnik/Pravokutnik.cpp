fefcd6c23dae434d1f0db25bf25f465a924e05ce
kkeglje <kegljevickarlo@gmail.com>
2018-03-08 19:16:56 +0100

    work in progress

---
+++ vjezbe1/Pravokutnik/Pravokutnik.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include "Pravokutnik.h"
+using namespace std;
+
+void Pravokutnik::get_sirina(int x){
+	sirina = x;
+}
+
+void Pravokutnik::get_visina(int y){
+	visina = y;
+}
+
+void Pravokutnik::upis_u_polje(){
+	for (int i = 0; i < 5; i++) {
+		cout << "Upisite sirinu:";
+		
+
+	}
+
+}
+
