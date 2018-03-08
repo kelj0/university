c4955db97902781ef92223c114534ce590fc6bc2
kkeglje <35850075+kkeglje@users.noreply.github.com>
2018-03-08 08:21:39 +0100

    Update ComplexNumber.cpp

--- dz1/dz1/ComplexNumber.cpp
+++ dz1/dz1/ComplexNumber.cpp
@@ -14,3 +14,8 @@ double ComplexNumber::get() {
 	std::cout << "Getting..\n";
 	return 10;
 };
+
+ComplexNumber::ComplexNumber(){};
+ComplexNumber::ComplexNumber(int real){};
+ComplexNumber::ComplexNumber(int real,int imaginary){};
+ComplexNumber::ComplexNumber(ComplexNumber k1,ComplexNumber k2){};
