20521036f9b707570b8c550276a1ce4747a07707
kkeglje <35850075+kkeglje@users.noreply.github.com>
2018-03-08 08:17:47 +0100

    Update ComplexNumber.h

--- dz1/dz1/ComplexNumber.h
+++ dz1/dz1/ComplexNumber.h
@@ -9,19 +9,10 @@ public:
 	void set_imaginary(int imaginary);
 	double get();
 
-	ComplexNumber() {
-		
-	};
-	ComplexNumber(int real) {
-
-	};
-	ComplexNumber(int real, int imaginary) {
-		
-	};
-	ComplexNumber(ComplexNumber k1, ComplexNumber k2) {
-		
-	};
-
+	ComplexNumber();
+	ComplexNumber(int real);
+	ComplexNumber(int real, int imaginary);
+	ComplexNumber(ComplexNumber k1, ComplexNumber k2);
 
 };
 
