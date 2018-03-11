0a7635b9791b5f956771d4da8bcce1e2059f6fd6
kkeglje <kegljevickarlo@gmail.com>
2018-03-11 19:42:08 +0100

    added private variables

--- dz1/ComplexNumber.cpp
+++ dz1/ComplexNumber.cpp
@@ -4,10 +4,12 @@ void set_real(){std::cout << "Setting real\n";}
 
 void ComplexNumber::set_real(int a) {
 	std::cout << "Setting real a->" << a << std::endl;
+	real = a;
 };
 
 void ComplexNumber::set_imaginary(int a) {
 	std::cout << "Setting imaginary a->" << a << std::endl;
+	imaginary = a;
 };
 
 double ComplexNumber::get() {
--- dz1/ComplexNumber.h
+++ dz1/ComplexNumber.h
@@ -19,6 +19,9 @@ public:
 		
 	};
 
+private:
+	int real;
+	int imaginary;
 
 };
 
