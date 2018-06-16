285d649710b052acf66d804228f8fe60965a7c94
Karlo Kegljevic <kegljevickarlo@gmail.com>
2018-06-16 23:56:11 +0200

    Update Zadatak1.cpp

--- Zadatak1/Zadatak1.cpp
+++ Zadatak1/Zadatak1.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-
 using namespace std::chrono_literals;
 using std::chrono::system_clock;
 
 int main() {
-	std::pair<int,int> a;
-	std::pair<int,int> b;
-	std::pair<int, int> x;
-	std::cout << "A redak: ";
-	std::cin >> a.first;
-	std::cout << "A stupac: ";
-	std::cin >> a.second;
-	std::cout << "B redak: ";
-	std::cin >> b.first;
-	std::cout << "B stupac: ";
-	std::cin >> b.second;
-
+	std::pair<int,int> a,b,x;
+	std::cout << "A redak: ";  std::cin >> a.first;
+	std::cout << "A stupac: "; std::cin >> a.second;
+	std::cout << "B redak: ";  std::cin >> b.first;
+	std::cout << "B stupac: "; std::cin >> b.second;
 	x.first = a.first;
 	x.second = a.second;
-
 	while (!(x.first == b.first&&x.second == b.second)) {
 		std::this_thread::sleep_until(system_clock::now() + 0.2s); //sleeps for 0.2s
 		if(!(x.first == b.first&&x.second == b.second))
