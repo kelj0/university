75d2d59f1fd40bad776a01c54373b08e08880a67
keljo <keljo@keljo.debian>
2019-06-12 11:09:57 +0200

    init

---
+++ csGUIapp/WindowsFormsWorldCup/DataLayer/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace DataLayer
+{ 
+    public class Data
+    {
+        private const string TeamsUrl    = "https://world-cup-json-2018.herokuapp.com/teams";
+        private const string MatchesUrl  = "https://world-cup-json-2018.herokuapp.com/matches/country?fifa_code=";
+        private const string ResultsUrl  = "https://world-cup-json-2018.herokuapp.com/teams/results";
+
+        public string GetUrl(string url)
+        {
+            var client = new RestClient(url);
+
+            var response = client.Execute(new RestRequest());
+
+            return response.Content;
+        }
+
+
+    }
+}
---
+++ csGUIapp/WindowsFormsWorldCup/WindowsFormsWorldCup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+
+namespace WindowsFormsWorldCup
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Data test = new Data();
+            Console.WriteLine(test.GetUrl("https://world-cup-json-2018.herokuapp.com/teams"));
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
